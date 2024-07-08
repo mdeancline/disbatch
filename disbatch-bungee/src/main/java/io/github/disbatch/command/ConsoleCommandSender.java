a54bf206e098cabea1511647d8c942abc36bd02e
Matthew Cline <mdeancline@gmail.com>
2024-07-08 10:35:38 -0500

    Clarify ConsoleCommandSender introduction

--- disbatch-bungee/src/main/java/io/github/disbatch/command/ConsoleCommandSender.java
+++ disbatch-bungee/src/main/java/io/github/disbatch/command/ConsoleCommandSender.java
@@ -7,6 +7,8 @@ import net.md_5.bungee.api.CommandSender;
  *
  * @apiNote The BungeeCord API has no way of exposing the console on its own. Therefore, the BungeeCord version
  * of Disbatch has its own implementations to work with the console.
+ *
+ * @since 1.0.0
  */
 public interface ConsoleCommandSender extends CommandSender {
 }
