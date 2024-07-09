ddf2c880642ae4602c3cc6c3f57236e3ed12f279
Matthew Cline <mdeancline@gmail.com>
2024-07-09 10:11:49 -0500

    Refactor internal CommandGroup operations

--- disbatch-bungee/src/main/java/io/github/disbatch/command/CommandGroup.java
+++ disbatch-bungee/src/main/java/io/github/disbatch/command/CommandGroup.java
@@ -4,6 +4,7 @@ import io.github.disbatch.command.descriptor.CommandDescriptor;
 import io.github.disbatch.command.exception.ArgumentIndexOutOfBoundsException;
 import io.github.disbatch.command.parameter.InvalidInputHandler;
 import io.github.disbatch.command.parameter.Parameter;
+import io.github.disbatch.command.parameter.ParameterUsages;
 import io.github.disbatch.command.parameter.ParameterizedCommand;
 import io.github.disbatch.command.parameter.decorator.MutableParameter;
 import net.md_5.bungee.api.CommandSender;
@@ -21,23 +22,22 @@ import java.util.StringJoiner;
  *
  * @since 1.0.0
  */
-public final class CommandGroup<S extends CommandSender> extends ParameterizedCommand<S, CommandGroup.GroupedCommandExecutor<? super S>> {
-    private final Map<String, GroupedCommand<? super S>> commands = new HashMap<>();
+public final class CommandGroup<S extends CommandSender> extends ParameterizedCommand<S, Command<? super S>> {
+    private final Map<String, Command<? super S>> commands = new HashMap<>();
+
+    public CommandGroup() {
+        this(ParameterUsages.withChevrons("Usage: %usage"));
+    }
 
     public CommandGroup(final @NotNull InvalidInputHandler<? super S> handler) {
         this(new MutableParameter<>(), handler);
     }
 
-    private CommandGroup(final MutableParameter<S, GroupedCommandExecutor<? super S>> parameter, final InvalidInputHandler<? super S> handler) {
+    private CommandGroup(final MutableParameter<S, Command<? super S>> parameter, final InvalidInputHandler<? super S> handler) {
         super(parameter, handler);
 
-        parameter.setUnderlyingParameter(new Parameter.Builder<S, GroupedCommandExecutor<? super S>>()
-                .parser((sender, input) -> {
-                    final GroupedCommand<? super S> groupedCommand = commands.get(input.getArgument(0));
-                    return groupedCommand == null
-                            ? null
-                            : new GroupedCommandExecutor<>(groupedCommand, input);
-                })
+        parameter.setUnderlyingParameter(new Parameter.Builder<S, Command<? super S>>()
+                .parser((sender, input) -> commands.get(input.getArgument(0)))
                 .tabCompleter(TabCompleters.forFirstArgument(TabCompleters.of(commands.keySet())))
                 .build());
     }
@@ -48,7 +48,7 @@ public final class CommandGroup<S extends CommandSender> extends ParameterizedCo
      * @param command    the {@code Command} to be linked
      * @param descriptor
      */
-    public CommandGroup<S> withCommand(final Command<? super S> command, final String label) {
+    public CommandGroup<S> withCommand(final @NotNull Command<? super S> command, final @NotNull String label) {
         return withCommand(command, new CommandDescriptor.Builder().label(label).build());
     }
 
@@ -58,126 +58,94 @@ public final class CommandGroup<S extends CommandSender> extends ParameterizedCo
      * @param command    the {@code Command} to be linked
      * @param descriptor
      */
-    public CommandGroup<S> withCommand(final Command<? super S> command, final CommandDescriptor descriptor) {
-        commands.put(descriptor.getLabel(), new GroupedCommand<>(command, descriptor.getLabel()));
+    public CommandGroup<S> withCommand(final @NotNull Command<? super S> command, final @NotNull CommandDescriptor descriptor) {
+        commands.put(descriptor.getLabel(), command);
 
         for (final String alias : descriptor.getAliases())
-            commands.put(alias, new GroupedCommand<>(command, alias));
+            commands.put(alias, command);
 
         return this;
     }
 
     @Override
-    protected void execute(final S sender, final GroupedCommandExecutor<? super S> executor, final CommandInput input) {
-        executor.execute(sender);
+    protected void execute(final S sender, final Command<? super S> command, final CommandInput input) {
+        command.execute(sender, new LazyLoadingGroupedCommandInput(input));
     }
 
-    private static class GroupedCommand<S extends CommandSender> {
-        private final Command<S> innerCommand;
-        private final String label;
+    private static class LazyLoadingGroupedCommandInput implements CommandInput {
+        private final CommandInput previous;
+        private final String cmdLabel;
+        private String[] arguments;
+        private String argumentLine;
 
-        GroupedCommand(final @NotNull Command<S> innerCommand, final String label) {
-            this.innerCommand = innerCommand;
-            this.label = label;
+        private LazyLoadingGroupedCommandInput(final CommandInput previous) {
+            this.previous = previous;
+            cmdLabel = previous.getCommandLabel() + " " + previous.getArgument(0);
         }
 
-        void execute(final S sender, final CommandInput input) {
-            innerCommand.execute(sender, input);
+        @Override
+        public int getArgumentLength() {
+            return previous.getArgumentLength() - 1;
         }
 
-        String getLabel() {
-            return label;
-        }
-    }
-
-    static class GroupedCommandExecutor<S extends CommandSender> {
-        private final GroupedCommand<S> command;
-        private final CommandInput input;
-
-        GroupedCommandExecutor(final GroupedCommand<S> command, final CommandInput input) {
-            this.command = command;
-            this.input = input;
+        @Override
+        public String getArgumentLine() {
+            return argumentLine == null
+                    ? (argumentLine = String.join(" ", getArguments()))
+                    : argumentLine;
         }
 
-        void execute(final S sender) {
-            command.execute(sender, new LazyLoadingGroupedCommandInput(input, command.getLabel()));
+        @Override
+        public String getArgument(final int index) {
+            final String[] arguments = getArguments();
+
+            if (index < 0 || index >= arguments.length)
+                throw new ArgumentIndexOutOfBoundsException(index);
+
+            return arguments[index];
         }
 
-        private static class LazyLoadingGroupedCommandInput implements CommandInput {
-            private final CommandInput previous;
-            private final String cmdLabel;
-            private String[] arguments;
-            private String argumentLine;
-
-            private LazyLoadingGroupedCommandInput(final CommandInput previous, final String recentCmdLabel) {
-                this.previous = previous;
-                cmdLabel = previous.getCommandLabel() + " " + recentCmdLabel;
-            }
-
-            @Override
-            public int getArgumentLength() {
-                return previous.getArgumentLength() - 1;
-            }
-
-            @Override
-            public String getArgumentLine() {
-                return argumentLine == null
-                        ? (argumentLine = String.join(" ", getArguments()))
-                        : argumentLine;
-            }
-
-            @Override
-            public String getArgument(final int index) {
-                final String[] arguments = getArguments();
-
-                if (index < 0 || index >= arguments.length)
-                    throw new ArgumentIndexOutOfBoundsException(index);
-
-                return arguments[index];
-            }
-
-            @Override
-            public String[] getArguments() {
-                if (arguments == null) {
-                    final String[] previousArguments = previous.getArguments();
-                    final String[] arguments = (this.arguments = new String[previousArguments.length - 1]);
-                    System.arraycopy(previousArguments, 1, arguments, 0, arguments.length);
-
-                    return arguments;
-                }
+        @Override
+        public String[] getArguments() {
+            if (arguments == null) {
+                final String[] previousArguments = previous.getArguments();
+                final String[] arguments = (this.arguments = new String[previousArguments.length - 1]);
+                System.arraycopy(previousArguments, 1, arguments, 0, arguments.length);
 
                 return arguments;
             }
 
-            @Override
-            public String getCommandLabel() {
-                return cmdLabel;
-            }
+            return arguments;
+        }
 
-            @Override
-            public String getCommandLine() {
-                return previous.getCommandLine();
-            }
+        @Override
+        public String getCommandLabel() {
+            return cmdLabel;
+        }
 
-            @Override
-            public boolean equals(final Object o) {
-                if (this == o) return true;
-                if (!(o instanceof CommandInput)) return false;
-                final CommandInput that = (CommandInput) o;
-                return getCommandLine().equals(that.getCommandLine());
-            }
+        @Override
+        public String getCommandLine() {
+            return previous.getCommandLine();
+        }
 
-            @Override
-            public int hashCode() {
-                return Objects.hash(getCommandLine());
-            }
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) return true;
+            if (!(o instanceof CommandInput)) return false;
+            final CommandInput that = (CommandInput) o;
+            return getCommandLine().equals(that.getCommandLine());
+        }
 
-            @Override
-            public String toString() {
-                return new StringJoiner(", ", getClass().getSimpleName() + "[", "]")
-                        .add("commandLine=" + getCommandLine())
-                        .toString();
-            }
+        @Override
+        public int hashCode() {
+            return Objects.hash(getCommandLine());
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", getClass().getSimpleName() + "[", "]")
+                    .add("commandLine=" + getCommandLine())
+                    .toString();
         }
     }
 }
