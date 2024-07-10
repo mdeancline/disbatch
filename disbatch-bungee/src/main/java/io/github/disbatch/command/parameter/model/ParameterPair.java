a923492bb9fb855b79326b7f0ecd428d2afccd4b
Matthew Cline <mdeancline@gmail.com>
2024-07-10 12:35:26 -0500

    Refactor handling of two parameters

--- disbatch-bungee/src/main/java/io/github/disbatch/command/parameter/model/ParameterPair.java
+++ disbatch-bungee/src/main/java/io/github/disbatch/command/parameter/model/ParameterPair.java
@@ -3,34 +3,47 @@ package io.github.disbatch.command.parameter.model;
 import io.github.disbatch.command.CommandInput;
 import io.github.disbatch.command.exception.ArgumentIndexOutOfBoundsException;
 import io.github.disbatch.command.parameter.Parameter;
-import org.bukkit.command.CommandSender;
+import io.github.disbatch.command.parameter.ParameterizedCommand;
+import net.md_5.bungee.api.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 
 /**
- * @deprecated use {@link ParameterPair} instead
+ * Allows the creation or retrieval of any two {@code Object}s, stored in a {@link Argument}, to be used in
+ * the execution phase of a {@link ParameterizedCommand}.
+ *
+ * @param <S> {@inheritDoc}
+ * @param <F> type of the first {@code Object} argument
+ * @param <L> type of the last {@code Object} argument
+ *
+ * @since 1.0.0
  */
-@Deprecated
-public final class PairedParameter<S extends CommandSender, F, L> implements Parameter<S, PairedArgument<F, L>> {
+public final class ParameterPair<S extends CommandSender, F, L> implements Parameter<S, ParameterPair.Argument<F, L>> {
     private final Parameter<? super S, F> first;
     private final Parameter<? super S, L> last;
 
-    public PairedParameter(final @NotNull Parameter<? super S, F> first, final @NotNull Parameter<? super S, L> last) {
+    /**
+     * Creates a new {@link ParameterPair}.
+     *
+     * @param first the first {@code Parameter} that will produce the first {@code Object} argument
+     * @param last  the last {@code Parameter} that will produce the last {@code Object} argument
+     */
+    public ParameterPair(final @NotNull Parameter<? super S, F> first, final @NotNull Parameter<? super S, L> last) {
         this.first = first;
         this.last = last;
     }
 
     @Override
-    public @Nullable PairedArgument<F, L> parse(final S sender, final CommandInput input) {
+    public @Nullable Argument<F, L> parse(final S sender, final CommandInput input) {
         final F firstResult = first.parse(sender, input);
         final L lastResult = last.parse(sender, new ReducedArgumentsInput(first, input));
 
         if (firstResult == null || lastResult == null)
             return null;
 
-        return new PairedArgument<>(firstResult, lastResult);
+        return new Argument<>(firstResult, lastResult);
     }
 
     @Override
@@ -50,12 +63,49 @@ public final class PairedParameter<S extends CommandSender, F, L> implements Par
         return Math.min(first.getMaximumUsage() + last.getMaximumUsage(), Integer.MAX_VALUE);
     }
 
-    static class ReducedArgumentsInput implements CommandInput {
+    /**
+     * Acts as a holder of the {@code Object} arguments created from any two {@code Parameter}s of a {@link ParameterPair}
+     * when a {@link ParameterizedCommand} is executed.
+     *
+     * @param <F> type of the first {@code Object} argument
+     * @param <L> type of the last {@code Object} argument
+     *
+     * @since 1.1.0
+     */
+    public static final class Argument<F, L> {
+        private final F first;
+        private final L last;
+
+        Argument(final F first, final L last) {
+            this.first = first;
+            this.last = last;
+        }
+
+        /**
+         * Retrieves the first {@code Object} argument.
+         *
+         * @return the first {@code Object} argument
+         */
+        public F getFirst() {
+            return first;
+        }
+
+        /**
+         * Retrieves the last {@code Object} argument.
+         *
+         * @return the last {@code Object} argument
+         */
+        public L getLast() {
+            return last;
+        }
+    }
+
+    private static class ReducedArgumentsInput implements CommandInput {
         private final Parameter<?, ?> first;
         private final CommandInput original;
         private String[] arguments;
 
-        ReducedArgumentsInput(final Parameter<?, ?> first, final CommandInput original) {
+        private ReducedArgumentsInput(final Parameter<?, ?> first, final CommandInput original) {
             this.first = first;
             this.original = original;
         }
--- disbatch-spigot/src/main/java/io/github/disbatch/command/parameter/model/PairedArgument.java
+++ disbatch-spigot/src/main/java/io/github/disbatch/command/parameter/model/PairedArgument.java
@@ -1,17 +1,10 @@
 package io.github.disbatch.command.parameter.model;
 
-import io.github.disbatch.command.parameter.ParameterizedCommand;
-
 /**
- * Acts as a holder of the {@code Object} arguments created from any two {@code Parameter}s of a {@link PairedParameter}
- * when a {@link ParameterizedCommand} is executed.
- *
- * @param <F> type of the first {@code Object} argument
- * @param <L> type of the last {@code Object} argument
- *
- * @since 1.0.0
+ * @deprecated see {@link PairedParameter}
  */
-public final class PairedArgument<F, L> {
+@Deprecated
+public class PairedArgument<F, L> {
     private final F first;
     private final L last;
 
@@ -20,20 +13,10 @@ public final class PairedArgument<F, L> {
         this.last = last;
     }
 
-    /**
-     * Retrieves the first {@code Object} argument.
-     *
-     * @return the first {@code Object} argument
-     */
     public F getFirst() {
         return first;
     }
 
-    /**
-     * Retrieves the last {@code Object} argument.
-     *
-     * @return the last {@code Object} argument
-     */
     public L getLast() {
         return last;
     }
